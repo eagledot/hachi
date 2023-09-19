6341414d3a2a1f3e64cd86fcfc2fd3407ef24aff
eagledot <usernainn@zohomail.in>
2023-09-19 16:40:36 +0530

    parse query to collect image attributes and their values

--- semantic_search.py
+++ semantic_search.py
@@ -8,3 +8,26 @@ def generate_endpoint(directory_path) -> str:
     statusEndpoint = os.path.abspath(directory_path).replace("/", "-")
     statusEndpoint = statusEndpoint.replace('\\',"-")
     return statusEndpoint
+
+def parse_query(query:str) -> dict[str, list[str]]:
+    """ parse a query.
+        a mapping from an image-attribute to a list with possible values.
+        "person" -> [x,y,z]
+    """
+    
+    temp_query = query.strip().lower().split(",")
+    imageAttributes_2_values = {}
+    for x in temp_query:
+        temp_x = x.strip().split(":")
+        attribute = temp_x[0].strip()
+
+        values = []
+        for v in temp_x[1].strip().split("-"):
+            if len(v.strip()) > 0:
+                values.append(v.strip())
+
+        if len(attribute) == 0 or len(values) == 0:
+            continue
+
+        imageAttributes_2_values[attribute] = values
+    return imageAttributes_2_values
