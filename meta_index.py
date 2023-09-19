cc3c079d0d0af0825deab6949facb99bfc6895ce
eagledot <usernainn@zohomail.in>
2023-09-19 17:52:58 +0530

    set of allowed data resources

--- meta_index.py
+++ meta_index.py
@@ -15,3 +15,9 @@ from fuzzy_search import FuzzySearch
 from reverse_geocode import GeocodeIndex
 import get_image_size
 
+ALLOWED_RESOURCES = {       
+    "audio": set([".mp3", ".aac"]),                              # TODO:
+    "video": set([".mp4", ".avi", ".mkv"]),                     # opencv should allow to read almost all type of video containers and codecs
+    "image": set([".jpg", ".jpeg", ".png", ".tiff", ".raw"]),   # opencv is being used to read raw-data, so almost all extensions are generally supported.
+    "text":  set([".pdf", ".txt", ".epub"])                     # TODO:
+}
