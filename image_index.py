86ccfb30fee715db05a5a1332091f33f1a494dca
eagledot <usernainn@zohomail.in>
2023-09-19 17:10:03 +0530

    subclassing parent Index class, mainly providing our own compare routine which use cosine distance

--- image_index.py
+++ image_index.py
@@ -12,3 +12,35 @@ from typing import Optional
 import numpy as np
 
 from sharding import CommonIndex
+
+class ImageIndex(CommonIndex):
+    def __init__(self, shard_size = IMAGE_SHARD_SIZE, embedding_size = IMAGE_EMBEDDING_SIZE, index_directory= IMAGE_INDEX_DIRECTORY, shard_prefix = SHARD_PREFIX, preload:bool = True) -> None:
+        super().__init__(
+            shard_prefix=shard_prefix,
+            index_directory=index_directory,
+            embedding_size=embedding_size,
+            shard_size=shard_size,
+            preload = preload
+        )
+    
+    def compare(self, query:np.array, data_embeddings:np.array) -> tuple[np.array, np.array]:
+        # return the sorted indices, and corresponding scores.
+
+        assert query.size == self.embedding_size, "Expected a single vector during query routine, use a For loop around query routine if needed."
+        assert len(data_embeddings.shape) == 2 , "Expected a 2D matrix"
+        assert data_embeddings.shape[1] == self.embedding_size
+
+        similarity_scores = np.matmul(data_embeddings, query.reshape((1, self.embedding_size)).transpose()) # [N,1]            
+        similarity_scores = similarity_scores.squeeze(1)
+        sorted_indices =  np.argsort(similarity_scores, axis = 0)[::-1]
+        sorted_scores = similarity_scores[sorted_indices]
+        return sorted_indices, sorted_scores
+    
+    def update(self, data_hash:str, data_embedding:np.array):
+        with self.lock:
+            self.update_base(data_hash, data_embedding = data_embedding)
+    
+    def query(self, query:np.array, client_key:str, key:Optional[list[str]] = None) -> tuple[bool, dict[str, list[float]]]:
+        with self.lock:
+            flag, hash_2_scores = self.query_base(query, client_key=client_key, key = key)
+            return flag, hash_2_scores
