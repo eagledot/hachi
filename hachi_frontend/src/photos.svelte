a35337a2e05c49a6f80192bd7fb4394844ca9588
eagledot <usernainn@zohomail.in>
2023-09-20 14:25:28 +0530

    imports exports

--- hachi_frontend/src/photos.svelte
+++ hachi_frontend/src/photos.svelte
@@ -1,8 +1,17 @@
 <script>
 
+import { createEventDispatcher, onDestroy, onMount } from 'svelte';
+import Filters from './filters.svelte';
+const dispatch = createEventDispatcher();  // attach dispatch to this instance. 
+export let show_exit_interface_button = false;    // in case to exit this component, depeneding on the parent. we can have a such button.
+
+export let image_data  = {
+            "list_metaData": [],
+            "list_dataHash": [],
+            "list_src": [],
+            "list_score": [],
+            "done":false
+    }
 
 
-
-
-    
 </script>
