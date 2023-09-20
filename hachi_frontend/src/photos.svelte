b301a71835564a4e2782dd2c91c444c38578dc5b
eagledot <usernainn@zohomail.in>
2023-09-20 17:27:52 +0530

    variables to hold state related data, change of image data would lead to sorting

--- hachi_frontend/src/photos.svelte
+++ hachi_frontend/src/photos.svelte
@@ -13,5 +13,39 @@ export let image_data  = {
             "done":false
     }
 
+    let filter_button_disabled = true;
+    let final_image_data = null;
+    $:if(image_data){
+      filter_button_disabled = !image_data.done
+      if(image_data.done){
+        final_image_data = image_data
+      }
+    }
+
+    onDestroy(() => {
+      image_data  = {
+        "list_metaData": [],
+        "list_dataHash": [],
+        "list_src": [],
+        "list_score": [],
+        "done":false}
+    })
+
+    let image_card_data = {} 
+    let meta_data_available = false; 
+    let sorted_scoreIndex = [];  
+
+    $: if (image_data){
+      sorted_scoreIndex = argsort(image_data.list_score);
+    }
+
+    let interface_state = {
+        // mutually exclusive states.
+        "parent":true,
+        "image_card":false,
+        "image_card_edit_interface":false,  // if editing meta-data for an image_card
+        "image_card_fullscreen":false
+    }
+
 
 </script>
