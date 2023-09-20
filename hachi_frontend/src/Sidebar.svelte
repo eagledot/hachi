03c390f9c48ccd75f82cab5918b107cb3aec6548
eagledot <usernainn@zohomail.in>
2023-09-20 13:53:16 +0530

    necessary state

---
+++ hachi_frontend/src/Sidebar.svelte
@@ -0,0 +1,44 @@
+<script>
+
+    import { createEventDispatcher, onMount } from 'svelte';
+    const dispatch = createEventDispatcher();  // attach dispatch to this instance. 
+
+    export let sidebarOpen = false;      // can control it from parent (generally used during initialization).
+    const state = {
+        sidebarOpen : sidebarOpen,
+    }
+
+    const menuItems = [
+		{
+			icon : "image",
+			path : "#",
+			name : "Photos",
+			count: 0
+		},
+		{
+			icon : "video",
+			path : "#",
+			name : "Videos",
+			count: 0
+		},
+		{
+			icon : "book-atlas",
+			path : "#",
+			name : "Index Library",
+			count: 0
+		},
+		{
+			icon : "user-tag",
+			path : "#",
+			name : "People",
+			count: 0
+		},
+		{
+			icon : "location-dot",
+			path : "#",
+			name : "Places",
+			count: 0
+		}
+
+	]
+
