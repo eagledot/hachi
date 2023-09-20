f71f13d881b571104142a9dcb718306dc84179dc
eagledot <usernainn@zohomail.in>
2023-09-20 13:37:50 +0530

    necessary svelte components

---
+++ hachi_frontend/src/App_search.svelte
@@ -0,0 +1,11 @@
+  
+<script>
+  // IMAGES INDEXING AND SEARCH INTERFACE.
+  import Fuzzy from "./Fuzzy.svelte"
+  import Sidebar from "./Sidebar.svelte"
+  import Indexing from "./Indexing.svelte"
+  import Photos from "./photos.svelte";
+  import People from "./people.svelte";
+  import Place from "./place.svelte";
+
+</script>
