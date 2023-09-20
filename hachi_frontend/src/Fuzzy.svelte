c4ef91ad88ccbd83fdb3d62c12d20f3352636a51
eagledot <usernainn@zohomail.in>
2023-09-20 21:10:23 +0530

    exports, declarations

--- hachi_frontend/src/Fuzzy.svelte
+++ hachi_frontend/src/Fuzzy.svelte
@@ -2,4 +2,35 @@
     Also contributed by akshay (@akshaymalik1995 gitlab).
  -->
 
- 
+<script>
+
+import { createEventDispatcher } from 'svelte';
+    // import Search from './lib/search.svelte';
+    const dispatch = createEventDispatcher();  // attach dispatch to this instance. 
+
+
+    let selectedOption = "query";
+    let input_element;
+    let query_button;
+
+    export let query_button_disabled = false;
+    $: if(query_button){
+        query_button.disabled = query_button_disabled  // supposed to update query_button.disabled property based on a variable.
+    }
+
+    let query_completed = "";
+    let dropdownItems = [];             // current dropDown specific option/Image-attribute selected.
+
+    // holding the current value for image attributes.
+    let selectedFilters = {
+        person: [],
+        query: [],
+        place: [],
+        filename: []
+    };
+
+    let valueInput = "";
+    let showDropdown = false;
+
+
+</script>
