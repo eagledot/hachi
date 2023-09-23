6b0b30cb5c26f143cf6d720580b31b71c3944c1c
eagledot <usernainn@zohomail.in>
2023-09-23 12:10:01 +0530

    more fine controlled process for indexing, collecting resources, and then indexing resources in batches, sending extra information during indexing status check, more responsive cancellation

--- semantic_search.py
+++ semantic_search.py
@@ -177,6 +177,111 @@ personId_to_avgEmbedding = {} # we seek to create average embedding for a group/
 prefix_personId =  "Id{}".format(str(time.time()).split(".")[0]).lower()   # a prefix to be used while assigning ids to unknown persons.( supposed to be unique enough)
 global_lock = threading.RLock()
 
+def indexing_thread(index_directory:str, client_id:str, include_subdirectories:bool = True, batch_size = 10, generate_preview_data:bool = True):
+
+    exit_thread = False
+    resource_mapping = collect_resources(index_directory, include_subdirectories)
+    
+    image_resources = resource_mapping["image"]    # collect a particular type of resources.
+    for (resource_directory, contents) in (image_resources.items()):
+        if exit_thread:
+            print("Finishing index on cancellation request from user")
+            break
+
+        # process the contents in batches.
+        count = 0
+        eta = "unknown"
+        while True:
+            contents_batch =  contents[count: count + batch_size]  # extract a batch
+            contents_batch = [os.path.join(resource_directory, x) for x in contents_batch]
+
+            if (len(contents_batch) == 0):    # should mean this directory has been 
+                break
+            
+            if indexStatus.is_cancel_request_active(client_id):
+                print("yes cancel request is active...")
+                exit_thread = True
+                break
+
+            # before each batch send the status to client
+            indexStatus.update_status(client_id, current_directory=resource_directory, progress = (count / len(contents) ), eta = eta, details = "{}/{}".format(count, len(contents)))
+
+            tic = time.time()         # start timing for this batch.
+            hash_2_metaData = metaIndex.extract_image_metaData(contents_batch)       # extra meta data
+            for data_hash, meta_data in hash_2_metaData.items():
+                assert data_hash is not None
+                absolute_path = meta_data["absolute_path"]
+                is_indexed = meta_data["is_indexed"]
+                if is_indexed:
+                    continue
+                
+                # generate image embeddings
+                image_embedding = generate_image_embedding(image_path=absolute_path, center_crop=False)
+                if image_embedding is None:
+                    print("Invalid data for {}".format(absolute_path))
+                    continue
+
+                face_bboxes, face_embeddings = generate_face_embedding(image_path=absolute_path)
+                if face_bboxes.shape[0] > 0:
+                    meta_data["face_bboxes"] = []
+                    for bbox in face_bboxes:
+                        x1 = str(bbox[0])
+                        y1 = str(bbox[1])
+                        x2 = str(bbox[2])
+                        y2 = str(bbox[3])
+                        meta_data["face_bboxes"].append([x1, y1, x2, y2])
+
+                    with global_lock:
+                        # assign each face_embedding to a group.
+                        for temp_embedding in face_embeddings:
+                            id_2_assign = None
+
+                            worst_score = 10
+                            for id in personId_to_avgEmbedding:
+                                avg_embedding = personId_to_avgEmbedding[id]
+                                _, temp_scores = compare_face_embeddings(temp_embedding, avg_embedding.reshape(1, -1))
+                                score = temp_scores.ravel().item()
+                                if score <= 1.12:    # be conservative.. (for now no reliable way to detect sunglasses, that harms the average embedding if included..)
+                                    if (score < worst_score):
+                                        worst_score = score
+                                        id_2_assign = id
+                                                        
+                            if id_2_assign is None:
+                                # if no match is found, we create a new id.
+                                id_2_assign = "{}_{}".format(prefix_personId, len(personId_to_avgEmbedding) + 1)
+                                personId_to_avgEmbedding[id_2_assign] = temp_embedding
+                            else:
+                                personId_to_avgEmbedding[id_2_assign] = np.concatenate([personId_to_avgEmbedding[id_2_assign].reshape(1,-1), temp_embedding.reshape(1,-1)], axis = 0).mean(axis = 0)
+
+                            if meta_data["person"] is not None:
+                                meta_data["person"].append(id_2_assign)
+                            else:
+                                meta_data["person"] = [id_2_assign]
+                else:
+                    meta_data["person"] = ["no person detected"]
+
+                # sync/update both the indices.
+                meta_data["is_indexed"] = True
+                metaIndex.update(data_hash, meta_data)
+                imageIndex.update(data_hash, data_embedding = image_embedding)
+                if generate_preview_data:
+                    generate_image_preview(data_hash, absolute_path)
+
+            count += len(contents_batch)
+
+            # calculate eta..
+            dt_dc = (time.time() - tic) / (len(contents_batch) + 1e-5)    # dt/dc
+            eta_in_seconds = dt_dc * (len(contents) - count)                  # eta (rate * remaining images to be indexed.)
+            eta_hrs = eta_in_seconds // 3600
+            eta_minutes = ((eta_in_seconds) - (eta_hrs)*3600 ) // 60
+            eta_seconds = (eta_in_seconds) - (eta_hrs)*3600 - (eta_minutes)*60
+            eta = "{}:{:02}:{:02}".format(int(eta_hrs), int(eta_minutes), int(eta_seconds))
+    
+    # finally save the index/db to disk.
+    metaIndex.save()
+    imageIndex.save()
+    indexStatus.set_done(client_id)
+    imageIndex.sanity_check()
 
 
 ############
