b9a61b32c5a6ed6a00894cd0168303667c5f4dd1
eagledot <usernainn@zohomail.in>
2023-11-13 11:11:49 +0530

    html markup for interface, allow uploading if no client secret, if in-active, allowing activate

--- hachi_frontend/src/GPhotos.svelte
+++ hachi_frontend/src/GPhotos.svelte
@@ -114,6 +114,35 @@ function handleClientUpload(event){
     reader.readAsText(event.target.files[0]);
 }
 
-
-
 </script>
+
+<div class = "flex w-full justify-center items-center">
+    {#if gClientInfo}
+        <p class="bg-gray-100 p-2 border-2 border-black">{gClientInfo["client_id"]}</p>
+        {#if gClientInfo["is_activated"]}
+            <p class="bg-green-300 p-2 border-2 border-black">Active</p>
+        {:else}
+            <p class="bg-rose-300 p-2 border-2 border-black">Inactive</p>
+        {/if}
+    {:else}
+        <div class = "border-2 flex-1 justify-center items-center bg-blue-200">
+            <p class= "p-4">
+                * Make sure you have created an OAuth credential at google Cloud-Console for this app and have enabled <b>Google Photos Api</b>. For details visit <b>https://developers.google.com/photos/library/guides/get-started</b><br>
+                * <b>App type</b> must be chosen as <b>Web app</b> and One of Redirect URIs must be set as <b> http://localhost:5000/api/OAuthCallback </b> in corresponding credential.<br>
+                * Except <b>You</b> and <b>Google</b> , <b>No-one</b> would have access to your personal data. 
+            </p>
+            <!-- allow uploading json file -->
+            <div class="flex items-center justify-center text-md p-2 bg-blue-300">
+                <label for="file">{label}</label>
+                <input type="file" accept = ".json" on:change={handleClientUpload}/>
+            </div>
+        </div>
+    {/if}
+    
+</div>
+
+{#if display_activation_button}
+    <div class="flex justify-center items-center m-3">
+        <button class="bg-blue-400 p-2 text-md disabled:bg-blue-200" on:click={activate}>Activate Google photos</button>
+    </div>
+{/if}
