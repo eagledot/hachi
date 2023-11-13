8eefed48b4639cd914bf7451efe1fbda25f67c74
eagledot <usernainn@zohomail.in>
2023-11-13 11:03:47 +0530

    polling activation status

--- hachi_frontend/src/GPhotos.svelte
+++ hachi_frontend/src/GPhotos.svelte
@@ -26,4 +26,24 @@ onMount(()=>{
     get_client_info();
 })
 
+function pollActivationStatus(){
+    //keep polling for account activation progress, every 2 seconds.
+    fetch("/api/statusGAuthFlow")
+    .then((response) => {
+        response.json()
+        .then((data) => {
+        let is_finished = data["finished"];
+        if(is_finished == false){
+            setTimeout(pollActivationStatus, 2000);
+        }
+        else{
+            display_activation_button = false;
+            alert("Account Activation: " + data["status"].toString());
+            get_client_info();
+        }
+    })
+    })
+}
+
+
 </script>
