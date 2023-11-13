0c1042cb6e4e800e6d70af207dd6c2dd43f7d8e7
eagledot <usernainn@zohomail.in>
2023-11-13 11:00:39 +0530

    google photos, getting client info on mount

---
+++ hachi_frontend/src/GPhotos.svelte
@@ -0,0 +1,26 @@
+<!-- Svelte component to manage Google Photos related functionalities. -->
+<script>
+
+let gClientInfo = null;
+let display_activation_button = false;
+
+function get_client_info(){
+    fetch("/api/gClientInfo")
+    .then((response) => {
+        response.json()
+        .then((data) => {
+            if(data["client_id_available"] === true)
+            {
+            gClientInfo = data;
+                if (data["is_activated"] == false){
+                    display_activation_button = true;
+                }
+            }
+            
+        })
+    }
+    )
+}
+
+
+</script>
