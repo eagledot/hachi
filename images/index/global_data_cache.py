0edeecb4383738a4f63c7537172a58015aee6469
eagledot <usernainn@zohomail.in>
2024-10-20 13:30:03 +0530

    comments for clarity, popitem last directly.. removed weird for loop

--- images/index/global_data_cache.py
+++ images/index/global_data_cache.py
@@ -3,11 +3,17 @@ import threading
 from threading import RLock
 from typing import Union, Optional, Iterable
 from collections import OrderedDict
+import os
 
 
 class GlobalDataCache(object):
+    """ A data-cache supposed to read image-resource in the background!
+    Still to actually benchmark the actual impact. may be put some stats on how many misses or hits.
+    OR later make it a separate Nim/C  module to actually leverage threads in the parallel if found useful enough!
+    """
 
-    def __init__(self, max_size = 100) -> None:
+    def __init__(self, max_size = 50) -> None:
+        # NOTE: for a large images like >= 10 mb, this would induce a spike in RAM usage.
         self.lock = RLock()
         self.cache = OrderedDict()                    # hash to raw-data mapping.
         self.max_size = max_size                      # to limit the number of (raw_data) values cache can hold. Soft limit.
@@ -28,17 +34,15 @@ class GlobalDataCache(object):
         
         threading.Thread(target = self._background_thread, args = (data_hash, absolute_path)).start()
 
-    def get(self, data_hash:str, absolute_path:Optional[str] = None):
-        # If data_hash could not be found, we would read it from the absolute_path.
+    def get(self, data_hash:str, absolute_path:Optional[os.PathLike] = None):
+        # If data_hash could not be found, we would read it from the absolute_path provided!
         with self.lock:
 
             if len(self.cache) >= self.max_size:
-                for key in self.cache.keys():   
-                    _ = self.cache.pop(key)    #pop least recent key.
-                    break
-
+                _ = self.cache.popitem(last = False)  # just keeps it from growing indefinitely..
+            
             if data_hash not in self.cache:
-                assert absolute_path is not None
+                # assert absolute_path is not None # open would give us the error anyway..
                 with open(absolute_path, "rb") as f:
                     raw_data = f.read()                
                 self.cache[data_hash] = raw_data
