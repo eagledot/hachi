558ae82708342e67a74f6bddc3dfd4714bce6673
eagledot <usernainn@zohomail.in>
2024-11-25 11:58:45 +0530

    sensible loading of meta-data on demand fo r each person

--- images/hachi_frontend/src/place.svelte
+++ images/hachi_frontend/src/place.svelte
@@ -12,18 +12,20 @@
     }
 
     // updated/fulfilled on mount.
-    let image_data = {
-        "list_metaData": [],
-        "list_score": [],
-        "list_dataHash": [],
-    }
+    // todo delete as redundant
+    // let image_data = {
+    //     "list_metaData": [],
+    //     "list_score": [],
+    //     "list_dataHash": [],
+    // }
 
     // place data to be fulfilled on the click. for chose place.
     let place_data = {
         "list_metaData": [],
         "list_score": [],
         "list_dataHash": [],
-        "done":false
+        "done":false,
+        "progress": 0
     }
 
 
@@ -31,23 +33,39 @@
     let photos_interface_active = false;
     let data_downloaded = true; // not exactly being used for now.. (may be used during destroy .. to prevent )
     onMount(() =>{
-        // we get the data for all the places at once only on mount. Needs to refresh to show very recent data or mount again.
-
-        data_downloaded = false;
-
+        
+        
         // get the corresponding data for all places.
         fetch("/api/getGroup/place").
         then((response) => {
             response.json().then((data) => {
-            let data_length = data["meta_data"].length;
-            for(let i = 0; i < data_length; i++){
-                image_data.list_dataHash.push(data["data_hash"][i]);
-                image_data.list_score.push(data["score"][i]);
-                image_data.list_metaData.push(data["meta_data"][i]);
-            }
+            
+            // just assume an array of unique values for person.
+            state.places = data;
             data_downloaded = true;
-            state.places = data["place"];
-            });
+            }) 
+        
+        
+        
+        
+        //////// older code /////////////////////
+        // we get the data for all the places at once only on mount. Needs to refresh to show very recent data or mount again.
+
+        // data_downloaded = false;
+
+        // // get the corresponding data for all places.
+        // fetch("/api/getGroup/place").
+        // then((response) => {
+        //     response.json().then((data) => {
+        //     let data_length = data["meta_data"].length;
+        //     for(let i = 0; i < data_length; i++){
+        //         image_data.list_dataHash.push(data["data_hash"][i]);
+        //         image_data.list_score.push(data["score"][i]);
+        //         image_data.list_metaData.push(data["meta_data"][i]);
+        //     }
+        //     data_downloaded = true;
+        //     state.places = data["place"];
+        //     });
         
         });
     })
@@ -56,33 +74,66 @@
         console.log("destroying...");
     })
 
-    async function handleClick(place_id){
+    function handleClick(place_id){
 
-        photos_interface_active = false;
+                // here handle clicking by getting the meta-data for a specific person!
+                fetch("/api/getMeta/place/" + place_id.toString()).
+        then((response) =>{
+            response.json().then((temp_meta_data) => {
+                photos_interface_active = false;
+                place_data = {
+                "list_metaData": [],
+                "list_score": [],
+                "list_dataHash": [],
+                "done":false,
+                "progress":0
+                }
+                
 
-        place_data = {
-        "list_metaData": [],
-        "list_score": [],
-        "list_dataHash": [],
-        "done":false
-        }
-
-        query_results_available.update((value) => structuredClone(place_data));
-
-        // update place_data for this place_id.
-        for(let i = 0; i<image_data.list_dataHash.length; i++){
-            
-            if(image_data.list_metaData[i]["place"].toLowerCase() === place_id.toLowerCase()){
-                place_data.list_metaData.push(image_data.list_metaData[i]);
-                place_data.list_dataHash.push(image_data.list_dataHash[i]);
-                place_data.list_score.push(image_data.list_score[i]);
-            }
-        }
-        place_data.done = true;  // should be enough to indicate svelte to render photos interface.
-        photos_interface_active = true;
+                let count = (temp_meta_data["data_hash"]).length // any key all have equal length.
+                console.log(count)
+                for(let i = 0; i<count; i++){
+                    // should be no need for this if condition .. as we collects meta data for this person id anyway!
+                        console.log('pushing tooooooo')
+                        place_data.list_metaData.push(temp_meta_data["meta_data"][i]);
+                        place_data.list_dataHash.push(temp_meta_data["data_hash"][i]);
+                        place_data.list_score.push(temp_meta_data["score"][i]);
+                    }
+                
+                place_data.done = true;  // should be enough to indicate svelte to render photos interface.
+                photos_interface_active = true;
+                query_results_available.update((value) => structuredClone(place_data));  // idk how much structuredClone effects but keep it so for now!
         
-        // indicate query
-        query_results_available.update((value) => structuredClone(place_data));
+                })
+        })
+        
+        
+        /// older code /////
+        // photos_interface_active = false;
+
+        // place_data = {
+        // "list_metaData": [],
+        // "list_score": [],
+        // "list_dataHash": [],
+        // "done":false
+        // }
+
+        // query_results_available.update((value) => structuredClone(place_data));
+
+        // // update place_data for this place_id.
+        // for(let i = 0; i<image_data.list_dataHash.length; i++){
+            
+        //     if(image_data.list_metaData[i]["place"].toLowerCase() === place_id.toLowerCase()){
+        //         place_data.list_metaData.push(image_data.list_metaData[i]);
+        //         place_data.list_dataHash.push(image_data.list_dataHash[i]);
+        //         place_data.list_score.push(image_data.list_score[i]);
+        //     }
+        // }
+        // place_data.done = true;  // should be enough to indicate svelte to render photos interface.
+        // photos_interface_active = true;
+        
+        // // indicate query
+        // query_results_available.update((value) => structuredClone(place_data));
     }
 
 // by default photos interface is not active,
