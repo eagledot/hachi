a50999a7deb537cab5e7c0d73fd979f4305eadf7
eagledot <usernainn@zohomail.in>
2025-10-22 11:53:56 +0530

    nuking svelte frontend

--- images/hachi_frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-// import './app.css'
-// import App from './App_search.svelte'
-import App from './App.svelte'
-
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
--- images/hachi_frontend/src/search.ts
+++
@@ -1,8 +0,0 @@
-// import './app.css'
-import App from './App_search.svelte'
-
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
